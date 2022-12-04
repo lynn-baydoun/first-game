85018edbd426b29c05c4a588ddfb7be00cafacd6
lynn baydoun <lkb07@mail.aub.edu>
2022-12-04 15:36:53 +0200

    .

--- index.js
+++ index.js
@@ -15,10 +15,20 @@ console.log(image);
 const playerImage = new Image();
 playerImage.src = './img/playerDown.png';
 console.log(playerImage);
-//because it takes very long for it to load and the code is being called instantly so the image is not loading to make it load we use this
 
+//because it takes very long for it to load and the code is being called instantly so the image is not loading to make it load we use this
 image.onload = () => {
-    context.drawImage(image, -1100, -500);
-    context.drawImage(playerImage, 0, 0);
+    context.drawImage(image, -980, -600);
+    context.drawImage(
+        playerImage,
+        0, //starting to crop the sprite sheet from the beginning (the left)
+        0, //
+        playerImage.width/4, //crop width
+        playerImage.height, // height 
+        canvas.width/2 - playerImage.width/2, 
+        canvas.height/2 - playerImage.height/2,
+        playerImage.width/4, //width of which the image will be rendered out at
+        playerImage.height, //height of which the image will be rendered out at
+        );
 }
 
