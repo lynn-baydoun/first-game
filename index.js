50516b35b335f0aea67affbdb8833da83e0e024d
lynn baydoun <lkb07@mail.aub.edu>
2022-12-07 18:39:58 +0200

    .

--- index.js
+++ index.js
@@ -70,6 +70,10 @@ class Sprite{
         this.position = position;
         this.image = image;
         this.frames = frames;
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max;
+            this.height = this.image.height;
+        }
     }
 
     draw(){
@@ -118,26 +122,37 @@ const keys = {
     }
 }
 
-const testBoundary = new Boundary({
-    position: {
-        x: 400,
-        y: 400
-    }
-})
-const movables = [background, testBoundary]
+const movables = [background, ...boundaries]
+
+function rectangularCollision({rectangle1, rectangle2}){
+    return(player.position.x + player.width >= rectangle2.position.x && 
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y)
+}
+
 function animate(){
     window.requestAnimationFrame(animate); //creates an infinite loop
     //we need to keep rerendering it as the animation is going
     background.draw();
-    testBoundary.draw();
     player.draw();
-    // boundaries.forEach(boundary => {
-    //     boundary.draw();
-    // })
+    //adding all the boundaries
+    boundaries.forEach(boundary => {
+        boundary.draw();
+        //detecting for collision
+        if(rectangularCollision({
+            rectangle1: player,
+            rectangle2: testBoundary
+        }))
+            // player.position.x + player.width >= testBoundary.position.x && 
+            // player.position.x <= testBoundary.position.x + testBoundary.width &&
+            // player.position.y <= testBoundary.position.y + testBoundary.height &&
+            // player.position.y + player.height >= testBoundary.position.y)
+            {
+            console.log("colliding");
+        }
+    })
     
-    //if(player.position.x + player.width)
-
-
     if(keys.w.pressed && lastKey === 'w') {
         movables.forEach(movable =>{
             movable.position.y += 3;
