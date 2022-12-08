b6b5ce0a066ccae15f593789e480383f5a8e8b58
lynn baydoun <lkb07@mail.aub.edu>
2022-12-08 22:25:19 +0200

    .

--- index.js
+++ index.js
@@ -9,19 +9,6 @@ const collisionsMap = []
 for(let i = 0; i < collisions.length; i+=70){ // 70 is the width of the map this is looping through the collision array in increments 
     collisionsMap.push(collisions.slice(i, 70 + i));
 }
-class Boundary{
-    static width = 48; //this is only in case i forget what 48 is for
-    static height = 48;
-    constructor({position}){
-        this.position = position;
-        this.width = 48; //don't forget that we imported this map 4 times its actual size 12x4
-        this.height = 48;
-    }
-    draw(){
-        context.fillStyle = 'red';
-        context.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-}
 
 const boundaries = []
 const offset ={
@@ -65,31 +52,6 @@ playerImage.src = './img/playerDown.png';
 //         playerImage.height, //height of which the image will be rendered out at
 //         );
 // }
-class Sprite{
-    constructor({position, velocity, image, frames = {max: 1}}) {
-        this.position = position;
-        this.image = image;
-        this.frames = frames;
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max;
-            this.height = this.image.height;
-        }
-    }
-
-    draw(){
-        context.drawImage(
-            this.image,
-            0, //starting to crop the sprite sheet from the beginning (the left)
-            0, //
-            this.image.width/this.frames.max, 
-            this.image.height, // height 
-            this.position.x, //the placement of our sprite
-            this.position.y, 
-            this.image.width/this.frames.max, //width of which the image will be rendered out at
-            this.image.height, //height of which the image will be rendered out at
-            );
-    }
-}
 
 const player = new Sprite({
     position:{
@@ -101,6 +63,13 @@ const player = new Sprite({
 })
 
 const background = new Sprite({
+    position: {
+        x: offset.x,
+        y: offset.y
+    },
+    image: image
+});
+const foreground = new Sprite({
     position: {
         x: offset.x,
         y: offset.y
