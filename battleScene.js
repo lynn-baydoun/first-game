68db07aeefcff2a9fd60356bc6e2b756f9f98645
lynn baydoun <lkb07@mail.aub.edu>
2022-12-24 06:29:47 +0200

    .

--- battleScene.js
+++ battleScene.js
@@ -10,79 +10,97 @@ const battleBackground = new Sprite({
 });
 
 //monsters
-const draggle = new Monster(monsters.Draggle);
-const emby = new Monster(monsters.Emby);
+let draggle;
+let emby;
 //we are using render so the animation and etc works within a timeline
-const renderedSprites = [draggle, emby];
+let renderedSprites;
+let battleAnimationId;
+//we created a Q array to Q all the attacks from the enemy and randomize it accordingly
+let queue;
 
-//adding both attack buttons
-emby.attacks.forEach((attack) => {
-  //the attack buttons:
-  const button = document.createElement("button");
-  button.innerHTML = attack.name;
-  document.querySelector("#attacksBox").append(button);
-});
+//to make sure the next battle loads in correctly
+function initBattle() {
+  draggle = new Monster(monsters.Draggle);
+  emby = new Monster(monsters.Emby);
+  renderedSprites = [draggle, emby];
+  queue = [];
 
+  //adding both attack buttons
+  emby.attacks.forEach((attack) => {
+    //the attack buttons:
+    const button = document.createElement("button");
+    button.innerHTML = attack.name;
+    document.querySelector("#attacksBox").append(button);
+    //event listeners for our attack buttons
+    document.querySelectorAll("button").forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const selectedAttack = attacks[e.currentTarget.innerHTML];
+        emby.attack({
+          attack: selectedAttack,
+          recipient: draggle,
+          renderedSprites,
+        });
+        //death rip draggle
+        if (draggle.health <= 0) {
+          queue.push(() => {
+            draggle.faint({});
+          });
+          queue.push(() => {
+            //fade to black
+            gsap.to("#overlappingDiv", {
+              opacity: 1,
+              onComplete: () => {
+                cancelAnimationFrame(battleAnimationId);
+                animate();
+                document.querySelector("#userinterface").style.display = "none";
+                gsap.to("#overlappingDiv", {
+                  opacity: 0,
+                });
+              },
+            });
+          });
+        }
+        //randomizing enemy attacks
+        const randomAttack =
+          draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)];
+
+        queue.push(() => {
+          draggle.attack({
+            attack: randomAttack,
+            recipient: emby,
+            renderedSprites,
+          });
+          //death rip emby
+          if (emby.health <= 0) {
+            queue.push(() => {
+              emby.faint({});
+            });
+          }
+        });
+      });
+
+      //changes the attack type display
+      button.addEventListener("mouseenter", (e) => {
+        const selectedAttack = attacks[e.currentTarget.innerHTML];
+        document.querySelector("#attackType").innerHTML = selectedAttack.type;
+        document.querySelector("#attackType").style.color =
+          selectedAttack.color;
+      });
+    });
+  });
+}
 //battle animation
 function animateBattle() {
-  window.requestAnimationFrame(animateBattle);
+  battleAnimationId = window.requestAnimationFrame(animateBattle);
   battleBackground.draw();
 
   renderedSprites.forEach((sprites) => {
     sprites.draw();
   });
 }
-
+initBattle();
 animateBattle();
-//we created a Q array to Q all the attacks from the enemy and randomize it accordingly
-const queue = [];
-//event listeners for our attack buttons
-document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", (e) => {
-    const selectedAttack = attacks[e.currentTarget.innerHTML];
-    emby.attack({
-      attack: selectedAttack,
-      recipient: draggle,
-      renderedSprites,
-    });
-    //death rip draggle
-    if (draggle.health <= 0) {
-      queue.push(() => {
-        draggle.faint({});
-      });
-      queue.push(() => {
-        //fade to black
-        gsap.to("#overlappingDiv", {
-          opacity: 1,
-        });
-      });
-    }
-    //randomizing enemy attacks
-    const randomAttack =
-      draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)];
 
-    queue.push(() => {
-      draggle.attack({
-        attack: randomAttack,
-        recipient: emby,
-        renderedSprites,
-      });
-      //death rip emby
-      if (emby.health <= 0) {
-        queue.push(() => {
-          emby.faint({});
-        });
-      }
-    });
-  });
-
-  //changes the attack type display
-  button.addEventListener("mouseenter", (e) => {
-    const selectedAttack = attacks[e.currentTarget.innerHTML];
-    document.querySelector("#attackType").innerHTML = selectedAttack.type;
-    document.querySelector("#attackType").style.color = selectedAttack.color;
-  });
-});
 //changing the dialogue box according to our enemy's attacks
 document.querySelector("#dialogueBox").addEventListener("click", (e) => {
   if (queue.length > 0) {
